4fa834186a0a2523af2c6ad33e7850e6a4ddef8c
Victor <medina.levic24@triana.salesianos.edu>
2024-01-11 09:02:38 +0100

    hfds

--- EjerciciosUD4_Parte01_Victor_Medina_Lejeune/src/ejercicio02/Documento.java
+++ EjerciciosUD4_Parte01_Victor_Medina_Lejeune/src/ejercicio02/Documento.java
@@ -2,5 +2,23 @@ package ejercicio02;
 
 public class Documento {
 
-	private 
+	protected String nombreEmpresa;
+	protected String nombreJefe;
+	protected String direccion;
+
+	public Documento(String nombreEmpresa, String nombreJefe, String direccion) {
+		super();
+		this.nombreEmpresa = nombreEmpresa;
+		this.nombreJefe = nombreJefe;
+		this.direccion = direccion;
+	}
+
+	public void imprimirDocumento() {
+		System.out.println("----------------------------------------------------");
+		System.out.println("Empresa:" + nombreEmpresa);
+		System.out.println("Equipo directivo:" + nombreJefe);
+		System.out.println("Dirección:" + direccion);
+		System.out.println("----------------------------------------------------");
+	}
+
 }
--- EjerciciosUD4_Parte01_Victor_Medina_Lejeune/src/ejercicio02/TarjetaDeVisita.java
+++ EjerciciosUD4_Parte01_Victor_Medina_Lejeune/src/ejercicio02/TarjetaDeVisita.java
@@ -1,5 +1,24 @@
 package ejercicio02;
 
-public class TarjetaDeVisita {
+public class TarjetaDeVisita extends Documento {
+
+	private String dni;
+	private int numeroContacto;
+
+	public TarjetaDeVisita(String nombreEmpresa, String nombreJefe, String direccion, String dni, int numeroContacto) {
+		super(nombreEmpresa, nombreJefe, direccion);
+		this.dni = dni;
+		this.numeroContacto = numeroContacto;
+	}
+
+	public void imprimirTarjeta() {
+		System.out.println("----------------------------------------------------");
+		System.out.println("Empresa:" + super.nombreEmpresa);
+		System.out.println("Equipo directivo:" + super.nombreJefe);
+		System.out.println("Dirección:" + super.direccion);
+		System.out.println("DNI:" + dni);
+		System.out.println("Número de contacto:" + numeroContacto);
+		System.out.println("----------------------------------------------------");
+	}
 
 }
